4a28cf1a1116849185a4fefdf0d66aa1d2d6e574
Nicholas Sebastian <nicholashendrata110900@gmail.com>
2023-01-18 03:03:32 +0700

    basic outline for the Customers page

--- src/pages/shipping/Customers.tsx
+++ src/pages/shipping/Customers.tsx
@@ -1,5 +1,30 @@
 import { FC } from "react";
+import styled from "styled-components";
+import { Form, Input, Button, Steps } from "antd";
+import { IInjectedProps as ViewProps } from "../../components/abstracts/withInitialData";
+import { IInjectedProps as FormProps } from "../../components/abstracts/withFormHandling";
 import TableTemplate from "../../components/compounds/TableTemplate";
+
+const { Item } = Form;
+const { Step } = Steps;
+
+const CustomersView: FC<ViewProps> = props => {
+  const { values } = props;
+  return (
+    <ViewContainer>
+      {/* TODO */}
+    </ViewContainer>
+  );
+}
+
+const CustomersForm: FC<FormProps> = props => {
+  const { initialValues, onSubmit } = props;
+  return (
+    <FormContainer>
+      {/* TODO */}
+    </FormContainer>
+  );
+}
 
 const Customers: FC = () => {
   return (
@@ -8,16 +33,17 @@ const Customers: FC = () => {
       columns={[
         { dataIndex: "", title: "" }
       ]}
-      view={[
-        { key: "", label: "" }
-      ]}
-      form={{
-        nameLabel: "Nama",
-        items: [
-          { key: "", label: "" }
-        ]
-      }} />
+      view={CustomersView}
+      form={CustomersForm} />
   );
 }
 
 export default Customers;
+
+const ViewContainer = styled.div`
+  // TODO
+`;
+
+const FormContainer = styled(Form)`
+  // TODO
+`;
