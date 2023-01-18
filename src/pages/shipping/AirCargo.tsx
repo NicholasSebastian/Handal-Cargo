8bf6bdab40377e581ccc83bd4c285a76dfb4ab76
Nicholas Sebastian <nicholashendrata110900@gmail.com>
2023-01-18 19:09:56 +0700

    created the structure for AirCargo and SeaFrieght pages

---
+++ src/pages/shipping/AirCargo.tsx
@@ -0,0 +1,47 @@
+import { FC } from "react";
+import styled from "styled-components";
+import { Form, Input, Button, Steps } from "antd";
+import TableTemplate from "../../components/compounds/TableTemplate";
+
+const { Item } = Form;
+const { Step } = Steps;
+
+const AirCargo: FC = () => {
+  return (
+    <TableTemplate // TODO
+      collectionName="AirCargo"
+      searchBy="name"
+      columns={[
+        { dataIndex: "", title: "" }
+      ]}
+      view={props => {
+        const { values } = props;
+        return (
+          <ViewContainer>
+            {/* TODO */}
+          </ViewContainer>
+        );
+      }}
+      form={props => {
+        const { initialValues, onSubmit } = props;
+        return (
+          <FormContainer
+            initialValues={initialValues}
+            onFinish={onSubmit}
+            labelCol={{ span: 7 }}>
+            {/* TODO */}
+          </FormContainer>
+        );
+      }} />
+  );
+}
+
+export default AirCargo;
+
+const ViewContainer = styled.div`
+  // TODO
+`;
+
+const FormContainer = styled(Form)`
+  // TODO
+`;
