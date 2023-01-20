afc235d1e0eba1b838985aa2e8e1f4b9a0fc44e8
Nicholas Sebastian <nicholashendrata110900@gmail.com>
2023-01-21 03:26:27 +0700

    implemented data backup feature

--- src/pages/admin/BackupRestore.tsx
+++ src/pages/admin/BackupRestore.tsx
@@ -1,5 +1,6 @@
-import {} from "@tauri-apps/api/fs";
 import { open } from '@tauri-apps/api/shell';
+import { writeTextFile, readTextFile } from "@tauri-apps/api/fs";
+import { open as select, save, SaveDialogOptions } from '@tauri-apps/api/dialog';
 import { FC, useState } from "react";
 import styled from "styled-components";
 import { Button, Alert, Transfer, message } from "antd";
@@ -27,6 +28,13 @@ const collections = [
   "StaffGroups"
 ];
 
+const options: SaveDialogOptions = {
+  filters: [{
+    name: 'JSON File',
+    extensions: ['json']
+  }]
+};
+
 const BackupRestore: FC = () => {
   const database = useDatabase();
   const [targetKeys, setTargetKeys] = useState<Array<string>>([]);
@@ -34,20 +42,27 @@ const BackupRestore: FC = () => {
 
   const onBackup = () => {
     setLoading(true);
-    Promise.all(targetKeys
-      .map(collection => database?.collection(collection)
-        .find()
-        .then(data => ({ collection, data }))
-      ))
-      .then(results => {
-        // TODO
-      })
+    Promise.all(targetKeys.map(collection => database?.collection(collection).find().then(data => ({ collection, data }))))
+      .then(results => save(options).then(filepath => ({ filepath, data: JSON.stringify(results) })))
+      .then(response => writeTextFile(response.filepath, response.data))
+      .then(() => message.success("Backup data berhasil disimpan."))
       .catch(() => message.error("Error terjadi ketika mengunduh backup data."))
       .finally(() => setLoading(false));
   }
 
   const onRestore = () => {
-    // TODO
+    setLoading(true);
+    select(options)
+      .then(filepath => {
+        if (filepath) return readTextFile(filepath as string);
+        else throw new Error();
+      })
+      .then(data => {
+        console.log(JSON.parse(data));
+        // TODO: replace all the included collections with the data.
+      })
+      .catch(() => message.error("Error terjadi ketika membaca backup data."))
+      .finally(() => setLoading(false));
   }
 
   return (
@@ -72,7 +87,7 @@ const BackupRestore: FC = () => {
           onChange={keys => setTargetKeys(keys)} />
         <div>
           <Button onClick={onBackup} loading={loading}>Backup</Button>
-          <Button onClick={onRestore}>Restore</Button>
+          <Button onClick={onRestore} loading={loading}>Restore</Button>
         </div>
       </div>
     </Container>
@@ -110,7 +125,7 @@ const Container = styled.div`
   }
 
   .ant-transfer {
-    width: calc(100% - 100px);
+    width: calc(100% - 120px);
   }
 
   .ant-transfer-list {
