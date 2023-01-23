20ac9bff30d47b70349bc48ac509a77b56acdf86
Nicholas <nicholashendrata110900@gmail.com>
2023-01-23 14:34:41 +0700

    implemented backup and restore

--- src/pages/admin/BackupRestore.tsx
+++ src/pages/admin/BackupRestore.tsx
@@ -1,9 +1,9 @@
 import { open } from '@tauri-apps/api/shell';
-import { writeTextFile, readTextFile } from "@tauri-apps/api/fs";
+import { writeFile, readTextFile } from "@tauri-apps/api/fs";
 import { open as select, save, SaveDialogOptions } from '@tauri-apps/api/dialog';
-import { FC, useState } from "react";
+import { FC, useState, useRef } from "react";
 import styled from "styled-components";
-import { Button, Alert, Transfer, message } from "antd";
+import { Button, Alert, Transfer, Modal, Progress, message } from "antd";
 import useDatabase from "../../data/useDatabase";
 
 const MONGODB_ATLAS_URL = "https://cloud.mongodb.com/";
@@ -39,30 +39,68 @@ const BackupRestore: FC = () => {
   const database = useDatabase();
   const [targetKeys, setTargetKeys] = useState<Array<string>>([]);
   const [loading, setLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const progressMax = useRef(0);
 
   const onBackup = () => {
+    // Prepare the modal to display the progress bar.
+    progressMax.current = targetKeys.length;
     setLoading(true);
-    Promise.all(targetKeys.map(collection => database?.collection(collection).find().then(data => ({ collection, data }))))
-      .then(results => save(options).then(filepath => ({ filepath, data: JSON.stringify(results) })))
-      .then(response => writeTextFile(response.filepath, response.data))
-      .then(() => message.success("Backup data berhasil disimpan."))
-      .catch(() => message.error("Error terjadi ketika mengunduh backup data."))
-      .finally(() => setLoading(false));
+
+    // Get all the data from the selected collections.
+    Promise.all(targetKeys.map(collection => database?.collection(collection).find()
+      .then(data => {
+        // Increment the progress bar.
+        setProgress(progress => progress + 1);
+        return { collection, data } as IBackupData;
+      }))
+    )
+    // Save the data into a file.
+    .then(results => save(options).then(filepath => ({ filepath, data: JSON.stringify(results) })))
+    .then(response => writeFile({ path: response.filepath, contents: response.data }))
+
+    // User feedback.
+    .then(() => message.success("Backup data berhasil disimpan."))
+    .catch(() => message.error("Error terjadi ketika mengunduh backup data."))
+    .finally(() => {
+      // Hide the modal and reset the progress bar.
+      setLoading(false);
+      setProgress(0);
+    });
   }
 
   const onRestore = () => {
-    setLoading(true);
+    // Choose a file and read the data from it.
     select(options)
       .then(filepath => {
         if (filepath) return readTextFile(filepath as string);
         else throw new Error();
       })
-      .then(data => {
-        console.log(JSON.parse(data));
-        // TODO: replace all the included collections with the data.
+      .then(contents => {
+        const data = JSON.parse(contents);
+
+        // Prepare the modal to display the progress bar.
+        progressMax.current = data.length;
+        setLoading(true);
+
+        // Replace the data's associated collections with our own.
+        return Promise.all(data.map((item: IBackupData) => 
+          database?.collection(item.collection).deleteMany({})
+            .then(() => database.collection(item.collection).insertMany(item.data)))
+            .then(() => setProgress(progress + 1))
+            .catch(console.error)
+            // TODO: Fix errors.
+          )
+          // User feedback.
+          .then(() => message.success(`${data.length} koleksi telah diubah.`))
+          .catch(() => message.error("Error terjadi ketika mengupload backup data."));
       })
       .catch(() => message.error("Error terjadi ketika membaca backup data."))
-      .finally(() => setLoading(false));
+      .finally(() => {
+        // Hide the modal and reset the progress bar.
+        setLoading(false);
+        setProgress(0);
+      });
   }
 
   return (
@@ -77,6 +115,15 @@ const BackupRestore: FC = () => {
           Dasbor Database
         </Button>
       </div>
+      <Modal centered 
+        visible={loading}
+        footer={null}
+        closable={false} 
+        maskClosable={false} >
+        <Progress 
+          percent={(progress / progressMax.current) * 100} 
+          status='active' />
+      </Modal>
       <div>
         <Transfer oneWay
           titles={['Semua', 'Akan di Backup']}
@@ -86,8 +133,8 @@ const BackupRestore: FC = () => {
           targetKeys={targetKeys}
           onChange={keys => setTargetKeys(keys)} />
         <div>
-          <Button onClick={onBackup} loading={loading}>Backup</Button>
-          <Button onClick={onRestore} loading={loading}>Restore</Button>
+          <Button onClick={onBackup}>Backup</Button>
+          <Button onClick={onRestore}>Restore</Button>
         </div>
       </div>
     </Container>
@@ -100,7 +147,6 @@ const Container = styled.div`
   background-color: #fff;
   margin: 20px;
   padding: 20px;
-  text-align: right;
   height: calc(100% - 40px);
 
   > div:first-child {
@@ -125,7 +171,7 @@ const Container = styled.div`
   }
 
   .ant-transfer {
-    width: calc(100% - 120px);
+    width: calc(100% - 100px);
   }
 
   .ant-transfer-list {
@@ -136,3 +182,8 @@ const Container = styled.div`
     height: calc(100% - 12px);
   }
 `;
+
+interface IBackupData {
+  collection: string
+  data: Array<any>
+}
