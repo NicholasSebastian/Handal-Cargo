e723caeaedd925fa4cb430ef09fe90d86b5a3024
Nicholas <nicholashendrata110900@gmail.com>
2023-02-12 18:44:51 +0700

    Some stuff for the InvoiceEntry page.

--- src/pages/shipping/InvoiceEntry/index.tsx
+++ src/pages/shipping/InvoiceEntry/index.tsx
@@ -3,11 +3,30 @@ import TableTemplate from "../../../components/compounds/TableTemplate";
 
 const InvoiceEntry: FC = () => {
   return (
-    <TableTemplate // TODO: Refer to page 36 on the design document.
+    <TableTemplate
       collectionName="Invoices"
       searchBy=""
+      query={(collectionName, search, searchBy) => {
+        // TODO
+        return undefined;
+      }}
       columns={[
-        { dataIndex: "", title: "" }
+        { dataIndex: "arrival_date", title: "Tanggal Tiba" },
+        { dataIndex: "muat_date", title: "Tanggal Muat" },
+        { dataIndex: "_id", title: "Kode Faktur" },
+        { dataIndex: "", title: "Kode Barang" },
+        { dataIndex: "container_number", title: "Nomor Container" },
+        { dataIndex: "marking", title: "Marking" },
+        { dataIndex: "quantity", title: "Kuantitas" },
+        { dataIndex: "measurement", title: "Ukuran" },
+        { dataIndex: "pricee", title: "Harga" },
+        { dataIndex: "volume_charge", title: "Cas Volume" },
+        { dataIndex: "handling_fee", title: "Biaya Ngurus" },
+        { dataIndex: "shipment_fee", title: "Ongkos Kirim" },
+        { dataIndex: "discount", title: "Diskon" },
+        { dataIndex: "other_fee", title: "Biaya Lain-Lain" },
+        { dataIndex: "total", title: "Total" },
+        { dataIndex: "description", title: "Keterangan" }
       ]}
       view={[
         { key: "", label: "" }
